e4a48a39f9c3eac1dbf587cbb20ae6e41b70b9f4
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2012-08-31 18:18:33 +0200

    First commit of project_code

---
+++ project_code/project_code.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Author: Stéphane Bidoul
+# Copyright (c) 2012 Acsone SA/NV (http://www.acsone.eu)
+# All Right Reserved
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from osv import osv
+
+class project(osv.osv):
+    _inherit = "project.project"
+
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        args = args[:]
+        ids = []
+        if name:
+            ids = self.search(cr, uid, [('code', '=like', name+"%")] + args, limit=limit)
+            if not ids:
+                ids = self.search(cr, uid, [('name', operator, name)] + args, limit=limit)
+        else:
+            ids = self.search(cr, uid, args, context=context, limit=limit)
+        return self.name_get(cr, uid, ids, context=context)
+    
+    def name_get(self, cr, uid, ids, context=None):
+        if not ids:
+            return []
+        reads = self.read(cr, uid, ids, ['name', 'code'], context=context)
+        res = []
+        for record in reads:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            res.append((record['id'], name))
+        return res
+    
+project()
+
+class account_analytic_account(osv.osv):
+    _inherit = 'account.analytic.account'
+    
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        args = args[:]
+        ids = []
+        if name:
+            ids = self.search(cr, uid, [('code', '=like', name+"%")] + args, limit=limit)
+            if not ids:
+                ids = self.search(cr, uid, [('name', operator, name)] + args, limit=limit)
+        else:
+            ids = self.search(cr, uid, args, context=context, limit=limit)
+        return self.name_get(cr, uid, ids, context=context)
+    
+    def name_get(self, cr, uid, ids, context=None):
+        if not ids:
+            return []
+        reads = self.read(cr, uid, ids, ['name', 'code'], context=context)
+        res = []
+        for record in reads:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            res.append((record['id'], name))
+        return res
+
+account_analytic_account()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
