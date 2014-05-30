3131e1f56b5f77fab1975d09b9b95fd7a78f3ef6
Laetitia Gangloff <laetitia.gangloff@acsone.eu>
2014-05-30 17:26:34 +0200

    add module update_std_price_account to change account to use for valuation when change standard price

---
+++ update_std_price_account/__openerp__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Authors: Laetitia Gangloff
+#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#    All Rights Reserved
+#
+#    WARNING: This program as such is intended to be used by professional
+#    programmers who take the whole responsibility of assessing all potential
+#    consequences resulting from its eventual inadequacies and bugs.
+#    End users who are looking for a ready-to-use solution with commercial
+#    guarantees and support are strongly advised to contact a Free Software
+#    Service Company.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Update standard price - account",
+    "version": "0.1",
+    "author": "ACSONE SA/NV",
+    "category": "Other",
+    "website": "http://www.acsone.eu",
+    "depends": ["stock",
+                ],
+    "description": """
+
+Update standard price - account
+=========================
+
+- When update standard price allow the selection of the account to use for stock valuation
+
+""",
+    "data": ['stock_change_standard_price_view.xml',
+              ],
+    "demo": [],
+    "test": [],
+    "active": False,
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
