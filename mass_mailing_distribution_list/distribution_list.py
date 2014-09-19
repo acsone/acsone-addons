ae6c8e2baeb930a772fa678b4ad0a802b657187a
Jonathan Nemry <jonathan.nemry@acsone.eu>
2014-09-19 19:31:58 +0200

    [mass_mailing_distribution_list][CHG] allow distribution list to manage newsletter with opt in/out and flag newsletter

--- mass_mailing_distribution_list/distribution_list.py
+++ mass_mailing_distribution_list/distribution_list.py
@@ -26,13 +26,31 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import orm
+from openerp.osv import orm, fields
 from openerp.tools import SUPERUSER_ID
 
 
 class distribution_list(orm.Model):
 
     _inherit = 'distribution.list'
+
+    _columns = {
+        'newsletter': fields.boolean('Newsletter'),
+        'partner_path': fields.char('Partner Path'),
+        'opt_out_ids': fields.many2many('res.partner',
+                                        'distribution_list_res_partner_out',
+                                        id1='distribution_list_id',
+                                        id2='partner_id', string='Opt-out'),
+        'opt_in_ids': fields.many2many('res.partner',
+                                       'distribution_list_res_partner_in',
+                                       id1='distribution_list_id',
+                                       id2='partner_id', string='Opt-in'),
+    }
+
+    _defaults = {
+        # default model is partner
+        'partner_path': 'id',
+    }
 
     def _register_hook(self, cr):
         """
@@ -47,3 +65,30 @@ class distribution_list(orm.Model):
         for model in res:
             mass_mailing_models.append(model[0])
         type(self).dst_model_id.domain = [('model', 'in', mass_mailing_models)]
+
+    def get_ids_from_distribution_list(
+            self, cr, uid, ids, safe_mode=True, context=None):
+        '''
+        manage opt in/out.
+        If the distribution list are newsletter and also have a parther_path
+        then
+        * remove all res_ids that contains a partner id into the opt_out_ids
+        * add to res_ids all partner id into the opt_in_ids
+        '''
+        res_ids = super(distribution_list, self).\
+            get_ids_from_distribution_list(cr, uid, ids, safe_mode=safe_mode,
+                                           context=context)
+        for dl in self.browse(cr, uid, ids, context=context):
+            if dl.newsletter and dl.partner_path:
+                partner_path = dl.partner_path
+                # manage opt result
+                dl_obj = self.pool[dl.dst_model_id.model]
+                not_opt_out_ids = dl_obj.search(
+                    cr, uid, [(partner_path, 'not in', [dl.opt_out_ids.id])],
+                    context=context)
+                res_ids = list(set(res_ids) & set(not_opt_out_ids))
+                in_ids = dl_obj.search(
+                    cr, uid, [(partner_path, 'in', [dl.opt_in_ids.id])],
+                    context=context)
+                res_ids = list(set(res_ids + in_ids))
+        return res_ids
