993bcc618892aff1966d3bfb7b9a17a357f84df6
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-01-31 18:30:01 +0100

    project_code: update license headers

--- project_code/project_code.py
+++ project_code/project_code.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Author: Stéphane Bidoul
-# Copyright (c) 2012 Acsone SA/NV (http://www.acsone.eu)
-# All Right Reserved
+#     This file is part of project_code, an Odoo module.
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsibility of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# guarantees and support are strongly advised to contract a Free Software
-# Service Company
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#     project_code is free software: you can redistribute it and/or
+#     modify it under the terms of the GNU Affero General Public License
+#     as published by the Free Software Foundation, either version 3 of
+#     the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#     project_code is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#     You should have received a copy of the
+#     GNU Affero General Public License
+#     along with project_code.
+#     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
