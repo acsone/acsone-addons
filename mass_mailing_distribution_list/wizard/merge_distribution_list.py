96ea0c526b57fec3159cf2ad072b629b0065f3e9
Olivier LAURENT (ACSONE) <olivier.laurent@acsone.eu>
2015-02-18 20:49:39 +0100

    Update merge_distribution_list.py
    [CHG] header

--- mass_mailing_distribution_list/wizard/merge_distribution_list.py
+++ mass_mailing_distribution_list/wizard/merge_distribution_list.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# This file is part of mass_mailing_distribution_list, an Odoo module.
+#     This file is part of mass_mailing_distribution_list, an Odoo module.
 #
-# Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-# mass_mailing_distribution_list is free software: you can redistribute it
-# and/or modify it under the terms of the GNU Affero General Public License
-# as published by the Free Software Foundation, either version 3 of
-# the License, or (at your option) any later version.
+#     mass_mailing_distribution_list is free software:
+#     you can redistribute it and/or
+#     modify it under the terms of the GNU Affero General Public License
+#     as published by the Free Software Foundation, either version 3 of
+#     the License, or (at your option) any later version.
 #
-# mass_mailing_distribution_list is distributed in the hope that it will
-# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+#     mass_mailing_distribution_list is distributed
+#     in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
 #
-# You should have received a copy of the
-# GNU Affero General Public License
-# along with distribution_list.
-# If not, see <http://www.gnu.org/licenses/>.
+#     You should have received a copy of the
+#     GNU Affero General Public License
+#     along with mass_mailing_distribution_list.
+#     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
