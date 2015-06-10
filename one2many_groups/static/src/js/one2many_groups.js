94e99407aaae6d8b85d70e0aa7e1d55ba6a6c0c8
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2015-06-10 11:17:57 +0200

    [ADD] css style in order to hide/show tree [CHG] * Add a level and notion of hierachy into the group managed by a click controller * Add the button to add group/remove group * Show padding within group level

--- one2many_groups/static/src/js/one2many_groups.js
+++ one2many_groups/static/src/js/one2many_groups.js
@@ -46,25 +46,60 @@ openerp.one2many_groups = function(instance) {
                             group_row = $(QWeb.render('TreeGrid.group_row',{
                                 group:group,
                             }));
+                            row_class = 'oe_group_level'+group.level;
                             if(group.parent_id){
                                 parent_row = self.$current.find('tr[data-group_id="'+group.parent_id[0]+'"]');
                                 parent_group_level = parent_row.attr('group_level');
-                                group_row.addClass('oe_group_level'+group.level);
+                                row_class = parent_row.attr('class') + ' ' + row_class;
+                                group_row.addClass(row_class);
                                 group_row.insertAfter(self.$current.find('.oe_group_level'+parent_group_level+':last'));
                             }
                             else{
-                                group_row.addClass('oe_group_level'+group.level);
+                                group_row.addClass(row_class);
                                 self.$current.prepend(group_row);
                             }
+                            vision_controller = self.$current.find('i#vision_controller'+group.id);
+                            vision_controller.addClass(row_class);
+                            self.init_controller_vision(group_row, vision_controller);
                             curr_last = group_row;
                             $.each(group.members_ids, function(key, id){
                                 curr = self.$current.find('tr[data-id='+id+']');
+                                curr.attr('row_level', group.level);
                                 curr.addClass(group_row.attr('class'));
                                 curr.insertAfter(curr_last);
                                 curr_last = curr;
                             });
                         });
                     }
+                },
+                init_controller_vision: function(row, el){
+                    var self = this;
+                    el.bind('click', function(event){
+                        vision_controller = $(event.target);
+                        row_class = 'oe_group_level' + row.attr('group_level');
+
+                        if(vision_controller.hasClass('fa-arrow-right')){
+                            // show only the same level elements
+                            vision_controller
+                                                .removeClass('fa-arrow-right')
+                                                .addClass('fa-arrow-down');
+                            row_level = row.attr('group_level');
+                            group_level = parseInt(row_level) + 1;
+                            elements = self.$current
+                                                    .find('tr[row_level="'+row_level+'"], tr[group_level="'+group_level+'"]');
+                            elements.removeClass('hidden');
+                        }
+                        else{
+                            // hide all sub-level
+                            self.$current.find('i.'+row_class)
+                                                                .removeClass('fa-arrow-down')
+                                                                .addClass('fa-arrow-right');
+                            elements = self.$current
+                                                        .find('tr.'+row_class)
+                                                        .not(self.$current.find('tr[data-group_id="'+row.attr('data-group_id')+'"]'));
+                            elements.addClass('hidden');
+                        }
+                    });
                 }
             });
 }
