67073e875dfdb616374e12882da99b041aa99ae1
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2015-10-01 18:55:08 +0200

    [IMP] embedded_picture

--- mail_embedded_picture/__openerp__.py
+++ mail_embedded_picture/__openerp__.py
@@ -27,10 +27,10 @@
     'summary': """
         This module set picture attachments as embedded part of mails
     """,
-    'author': "ACSONE SA/NV",
+    'author': "ACSONE SA/NV, Odoo Community Association (OCA)",
     'website': "http://acsone.eu",
     'category': 'Mail',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'mass_mailing',
--- mail_embedded_picture/models/ir_mail_server.py
+++ mail_embedded_picture/models/ir_mail_server.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#     This file is part of html_widget_embedded_picture, an Odoo module.
+#     This file is part of mail_embedded_picture, an Odoo module.
 #
 #     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-#     html_widget_embedded_picture is free software:
+#     mail_embedded_picture is free software:
 #     you can redistribute it and/or
 #     modify it under the terms of the GNU Affero General Public License
 #     as published by the Free Software Foundation, either version 3 of
 #     the License, or (at your option) any later version.
 #
-#     html_widget_embedded_picture is distributed
+#     mail_embedded_picture is distributed
 #     in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -19,7 +19,7 @@
 #
 #     You should have received a copy of the
 #     GNU Affero General Public License
-#     along with html_widget_embedded_picture.
+#     along with mail_embedded_picture.
 #     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
