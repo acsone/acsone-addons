d7976cd82b166767b210b02b2d86458fcae4637f
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-10-15 10:37:13 +0200

    [FIX][global_resource_leave] Fix domain

--- global_resource_leave/tests/test_global_resource_leave.py
+++ global_resource_leave/tests/test_global_resource_leave.py
@@ -34,11 +34,29 @@ class TestGlobalResourceLeave(common.TransactionCase):
         self.calendar_leave01 = \
             self.env.ref('resource.resource_analyst_leaves_demoleave1')
         self.global_leave01 = self.env.ref('resource.resource_dummyleave')
-        self.company02 = self.env.ref('multi_company.res_company_odoo')
+        company_obj = self.env['res.company']
+        partner_obj = self.env['res.partner']
+        partner = partner_obj.create(
+            {"name": "Test",
+             "is_company": True,
+             "email": "test@tes.ttest",
+             })
+        self.company02 = company_obj.create(
+            {"name": "Test",
+             "partner_id": partner.id,
+             "rml_header1": "My Company Tagline",
+             "currency_id": self.ref("base.EUR")
+             })
 
     def test_global_leave(self):
         # I ensure there isn't calendar on the leave
         self.assertFalse(self.global_leave01.calendar_id)
+        # I check if the global leave is not in the leaves on the calendar
+        self.assertFalse(self.global_leave01.id in
+                         self.calendar01.leave_ids.ids)
+        # I remove the resource on the leave
+        self.global_leave01.resource_id = False
+        self.calendar01.invalidate_cache()
         # I check if the global leave is in the leaves on the calendar
         self.assertTrue(self.global_leave01.id in
                         self.calendar01.leave_ids.ids)
