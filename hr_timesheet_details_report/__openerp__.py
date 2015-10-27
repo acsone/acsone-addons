d1b39d84501fdbf1049e155ee2ce17f0e59ab7ed
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-10-27 18:54:39 +0100

    [ADD] Add hr_timesheet_details_report module

---
+++ hr_timesheet_details_report/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#     This file is part of hr_timesheet_details_report,
+#     an Odoo module.
+#
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#
+#     hr_timesheet_details_report is free software:
+#     you can redistribute it and/or modify it under the terms of the GNU
+#     Affero General Public License as published by the Free Software
+#     Foundation,either version 3 of the License, or (at your option) any
+#     later version.
+#
+#     hr_timesheet_details_report is distributed
+#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with hr_timesheet_details_report.
+#     If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "HR timesheet details report",
+
+    'summary': """
+        Add timesheet details report""",
+    'author': 'ACSONE SA/NV',
+    'website': "http://acsone.eu",
+    'category': 'Reporting',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr_timesheet_task',
+        'project',
+    ],
+    'data': [
+        'wizard/hr_timesheet_details_report_wizard_view.xml',
+        'report/hr_timesheet_details_report.xml',
+    ],
+}
