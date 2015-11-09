a8c10e98da8707c98db7492c908e02a2f5510028
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-11-09 11:53:49 +0100

    [ADD] Refresh parent form on click on o2n button in the kanban view

---
+++ workflow_task/static/src/js/kanban.js
@@ -0,0 +1,22 @@
+openerp.workflow_task = function(instance) {
+
+instance.web_kanban.KanbanRecord.include({
+
+    do_reload: function() {
+        res = this._super();
+        var self = this;
+        if (self.view.m2m != 'undefined' && self.view.m2m.dataset.ids.length > 0 && self.view.m2m.options.reload_on_button) {
+            parent_form = self.view.m2m.view
+            $.when().then(function () {
+                if (parent_form)
+                    return parent_form.save();
+                else
+                    return $.when();
+            }).done(function () {
+                self.view.m2m.view.reload();
+            });
+        }
+        return res
+    }
+});
+};
