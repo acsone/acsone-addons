7784455be76399209af616cb8db5420329c77a2b
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-07-25 17:46:55 +0200

    [MIF] worfklow_x: Migrate to 10.0

--- workflow_task/__manifest__.py
+++ workflow_task/__manifest__.py
@@ -1,48 +1,49 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#     This file is part of workflow_activity_action,
+#     This file is part of workflow_task,
 #     an Odoo module.
 #
 #     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-#     workflow_activity_action is free software:
+#     workflow_task is free software:
 #     you can redistribute it and/or modify it under the terms of the GNU
 #     Affero General Public License as published by the Free Software
 #     Foundation,either version 3 of the License, or (at your option) any
 #     later version.
 #
-#     workflow_activity_action is distributed
+#     workflow_task is distributed
 #     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
 #     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 #     PURPOSE.  See the GNU Affero General Public License for more details.
 #
 #     You should have received a copy of the GNU Affero General Public License
-#     along with workflow_activity_action.
+#     along with workflow_task.
 #     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
-    'name': "Workflow activity action",
+    'name': "Workflow tasks",
 
     'summary': """
-        Manage object action from its activities""",
+        Automatically create tasks related to workflow activities""",
+
     'author': 'ACSONE SA/NV',
     'website': "http://acsone.eu",
+
     'category': 'Technical Settings',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'base',
-        'base_suspend_security',
+        'mail',
+        'workflow_activity_action',
     ],
     'data': [
         'security/ir.model.access.csv',
+        'security/workflow_task_security.xml',
         'views/wkf_activity.xml',
-        'views/workflow_activity_action.xml',
-        'views/activity_record_rule_view.xml',
-    ],
-    'qweb': [
-        'static/src/xml/base.xml',
+        'views/wkf_task_view.xml',
+        'views/workflow_task.xml',
     ],
 }
