f77ca165b430354dee3d51ba6ddfed7c95a8f705
gilles <meyomesse.gilles@gmail.com>
2017-03-08 17:39:25 +0100

    [MIG] resource_calendar_half_day

--- resource_calendar_half_day/README.rst
+++ resource_calendar_half_day/README.rst
@@ -15,6 +15,7 @@ Contributors
 ------------
 
 * Adrien Peiffer <adrien.peiffer@acsone.eu>
+* Meyomesse Gilles <meyomesse.gilles@gmail.com>
 
 Maintainer
 ----------
--- resource_calendar_half_day/models/resource.py
+++ resource_calendar_half_day/models/resource.py
@@ -1,29 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#     This file is part of resource_calendar_half_day,
-#     an Odoo module.
-#
-#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#     resource_calendar_half_day is free software:
-#     you can redistribute it and/or modify it under the terms of the GNU
-#     Affero General Public License as published by the Free Software
-#     Foundation,either version 3 of the License, or (at your option) any
-#     later version.
-#
-#     resource_calendar_half_day is distributed
-#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#     PURPOSE.  See the GNU Affero General Public License for more details.
-#
-#     You should have received a copy of the GNU Affero General Public License
-#     along with resource_calendar_half_day.
-#     If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 PERIOD_SELECTION = [('morning', 'Morning'),
                     ('afternoon', 'Afternoon')]
@@ -32,7 +11,9 @@ PERIOD_SELECTION = [('morning', 'Morning'),
 class ResourceCalendarAttendance(models.Model):
     _inherit = "resource.calendar.attendance"
 
-    periodofday = fields.Selection(selection=PERIOD_SELECTION,
-                                   string='Period Of Day',
-                                   default='morning',
-                                   required=True)
+    periodofday = fields.Selection(
+        string='Period Of Day',
+        selection=PERIOD_SELECTION,
+        default='morning',
+        required=True
+    )
