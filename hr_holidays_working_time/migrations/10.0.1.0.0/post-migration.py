2c6e65ad6c512ea7c0cbbd704933e68c4a5974a2
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-07-19 11:56:25 +0200

    [FIX] Missing migration part.

--- hr_holidays_working_time/migrations/10.0.1.0.0/post-migration.py
+++ hr_holidays_working_time/migrations/10.0.1.0.0/post-migration.py
@@ -12,3 +12,8 @@ def migrate(cr, version):
         "number_of_hours_temp=tmp_number_of_hours_temp")
     cr.execute(
         """ALTER TABLE hr_holidays DROP COLUMN "tmp_number_of_hours_temp" """)
+    cr.execute(
+        """update hr_holidays set set_hours_manually='t' where type='add'""")
+    cr.execute("update hr_holidays set "
+               "number_of_hours_temp_manual=number_of_hours_temp where "
+               "type='add'")
