0294120546ca929f06a2ba58c91a347ce56ce126
Denis Roussel <denis.roussel@acsone.eu>
2020-04-15 15:59:00 +0200

    [10.0][ADD] cagnotte_sale: Add module to manage cagnotte on sale

---
+++ cagnotte_sale/models/sale_order_line.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV (http://www.acsone.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    account_cagnotte_id = fields.Many2one(
+        comodel_name='account.cagnotte',
+        ondelete='restrict',
+        index=True,
+    )
+
+    @api.model
+    def create(self, vals):
+        res = super(SaleOrderLine, self).create(vals)
+        if 'account_cagnotte_id' not in vals:
+            res.order_id._reapply_cagnotte()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        """
+        We reapply cagnotte if needed. We don't do it in create() as write() is
+        called just after
+        :param vals:
+        :return:
+        """
+        res = super(SaleOrderLine, self).write(vals)
+        self.mapped('order_id')._reapply_cagnotte()
+        return res
+
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        if self.account_cagnotte_id:
+            self.name = self.account_cagnotte_id.name
+        return res
