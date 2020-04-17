5260738bbee6ae90406ddf11cf75d32c7fb3ffce
Denis Roussel <denis.roussel@acsone.eu>
2020-04-17 12:55:12 +0200

    [10.0][IMP] cagnotte_sale: Change the solde computation, add wizard
    As the balance of cagnotte should take into account not invoiced
    sale orders, we need to identify them and deduct the applied
    cagnotte amount to the cagnotte balance until the account move
    lines are generated.
    
    Add also a wizard to help paying with a cagnotte balance. This
    wizard is accessible only for those in the good group.

--- cagnotte_sale/models/sale_order.py
+++ cagnotte_sale/models/sale_order.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from odoo.fields import first
-from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
@@ -20,7 +19,15 @@ class SaleOrder(models.Model):
 
     @api.multi
     def _reapply_cagnotte(self):
-        for sale in self.filtered('has_cagnotte'):
+        """
+        If sale order contains a line with cagnotte, save it, unlink the
+        corresponding lines and then apply cagnotte.
+        This is typically used when sale order lines have changed and we
+        need to check if the cagnotte is still applicable.
+        :return:
+        """
+        for sale in self.filtered(
+                lambda s: s.state == 'draft' and s.has_cagnotte):
             cagnotte_line = first(
                 self.mapped('order_line').filtered('account_cagnotte_id'))
             cagnotte = cagnotte_line.account_cagnotte_id
@@ -46,19 +53,29 @@ class SaleOrder(models.Model):
         :param cagnotte:
         :return:
         """
-        if self.amount_total >= cagnotte.solde_cagnotte:
+        if self.amount_untaxed >= cagnotte.solde_cagnotte:
             return cagnotte.solde_cagnotte
-        return self.amount_total
+        return self.amount_untaxed
 
     @api.multi
     def unset_cagnotte(self):
         self.mapped('order_line').filtered('account_cagnotte_id').unlink()
 
+    def _get_cagnotte_applicable_sales(self):
+        """
+        We cannot apply a cagnotte to a confirmed sale order.
+        :return:
+        """
+        return self.filtered(lambda s: s.state == 'draft')
+
     @api.multi
     def apply_cagnotte(self, cagnotte):
+        """
+        Entry point on sale order level to apply cagnotte balance
+        :param cagnotte:
+        :return:
+        """
         for sale in self:
-            if cagnotte.partner_id != sale.partner_id:
-                raise UserError(_('The cagnotte you try to use is not yours!'))
             # We check that the sale order value is > 0
             if cagnotte.solde_cagnotte > 0.0 and self.amount_total > 0.0:
                 sale._generate_cagnotte_line(cagnotte)
@@ -71,19 +88,45 @@ class SaleOrder(models.Model):
             'product_uom_qty': -1.0,
             'product_uom': cagnotte.cagnotte_type_id.product_id.uom_id.id,
             'order_id': order.id,
-            'price_unit': self._get_cagnotte_usable_amount(cagnotte),
             'account_cagnotte_id': cagnotte.id,
         }
         return vals
 
+    def _get_cagnotte_line_price(self, line, cagnotte):
+        price_unit = self._get_cagnotte_usable_amount(cagnotte)
+        return price_unit
+
     @api.multi
     def _generate_cagnotte_line(self, cagnotte):
+        """
+        Generate sale order line corresponding to cagnotte product
+        :param cagnotte:
+        :return:
+        """
         self.ensure_one()
         vals = self._prepare_cagnotte_line(self, cagnotte)
         line = self.env['sale.order.line'].new(vals)
         line.product_id_change()
         vals = line._convert_to_write(line._cache)
         vals.update({
-            'price_unit': self._get_cagnotte_usable_amount(cagnotte),
+            'price_unit': self._get_cagnotte_line_price(line, cagnotte),
         })
         self.env['sale.order.line'].create(vals)
+
+    @api.multi
+    def cagnotte_pay(self):
+        """
+        Action for wizard to pay with cagnotte
+        :return:
+        """
+        action_rec = self.env.ref(
+            'cagnotte_sale.action_view_sale_cagnotte_pay')
+        if action_rec:
+            action = action_rec.read([])[0]
+            action['views'] = [
+                (view_id, mode) for (view_id, mode) in
+                action['views'] if mode == 'form'] or action['views']
+            action['context'] = {
+                'default_sale_order_id': self.id,
+            }
+            return action
