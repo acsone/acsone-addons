25e7522c93a8b73a6ac0c89cda0d504b9a133c59
Denis Roussel <denis.roussel@acsone.eu>
2020-04-22 11:43:36 +0200

    [10.0][IMP] cagnotte_base: Create move line with invoice one
    When the cagnotte is filled in on invoice line level,
    the generated move is filled in with the cagnotte too.

--- cagnotte_base/models/account_invoice.py
+++ cagnotte_base/models/account_invoice.py
@@ -21,3 +21,27 @@ class AccountInvoice(models.Model):
     def onchange_cagnotte_type_id(self):
         if self.cagnotte_type_id:
             self.account_id = self.cagnotte_type_id.account_id
+
+    def invoice_line_move_line_get(self):
+        """
+        Create move line with cagnotte id if needed
+        :return:
+        """
+        res = super(AccountInvoice, self).invoice_line_move_line_get()
+        cagnotte_lines = self.invoice_line_ids.filtered("account_cagnotte_id")
+        for line_val in res:
+            invl_id = line_val.get("invl_id")
+            if invl_id in cagnotte_lines.ids:
+                line_val.update({
+                    "account_cagnotte_id": cagnotte_lines.filtered(
+                        lambda c, l_id=invl_id: c.id == l_id).mapped(
+                        "account_cagnotte_id").id})
+        return res
+
+    @api.model
+    def line_get_convert(self, line, part):
+        res = super(AccountInvoice, self).line_get_convert(line, part)
+        cagnotte_id = line.get("account_cagnotte_id")
+        if cagnotte_id:
+            res.update({"account_cagnotte_id": cagnotte_id})
+        return res
