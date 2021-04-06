02f91376ed75dac6c368b6a7c61f6d6d59923a49
Denis Roussel <denis.roussel@acsone.eu>
2021-04-06 07:11:52 +0200

    [10.0][ADD] cagnotte_no_negative

---
+++ cagnotte_no_negative/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Cagnotte Limit',
+    'summary': """
+        Allows to set a limit on cagnotte use""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu',
+    'depends': [
+        "cagnotte_base",
+    ],
+}
