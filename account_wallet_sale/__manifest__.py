9ae66a72ef106b076971de8557f90e4ef1da4509
Denis Roussel <denis.roussel@acsone.eu>
2021-06-15 11:02:39 +0200

    [14.0][MIG] account_wallet_sale

---
+++ account_wallet_sale/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Wallet Sale',
+    'summary': """
+        Allows to manage wallet on sale level""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu',
+    'depends': [
+        'sale',
+        "account_wallet",
+    ],
+    'data': [
+        'security/security.xml',
+        'views/account_wallet.xml',
+        'views/sale_order.xml',
+        'wizards/sale_wallet_pay.xml',
+    ],
+}
