469e543e14676eb1418a18728102e730010d73e4
Denis Roussel <denis.roussel@acsone.eu>
2021-07-28 10:47:38 +0200

    [14.0][IMP] account_wallet_sale: Add migration scripts

---
+++ account_wallet_sale/hooks.py
@@ -0,0 +1,19 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, SUPERUSER_ID
+from openupgradelib import openupgrade
+
+
+def _rename_cagnotte(env):
+    columns = {
+        "sale_order_line": [
+            ("account_cagnotte_id", "account_wallet_id"),
+        ],
+    }
+    openupgrade.rename_columns(env.cr, columns)
+
+
+def pre_init_hook(cr):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        _rename_cagnotte(env)
