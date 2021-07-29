5d378bf17759447c537b676432b5e80d0aaa28eb
Denis Roussel <denis.roussel@acsone.eu>
2021-07-29 15:34:02 +0200

    [14.0][IMP] account_wallet: Remove former methods as models have been merged

--- account_wallet/models/account_move.py
+++ account_wallet/models/account_move.py
@@ -4,7 +4,7 @@
 from odoo import api, fields, models
 
 
-class AccountInvoice(models.Model):
+class AccountMove(models.Model):
     _inherit = "account.move"
 
     account_wallet_type_id = fields.Many2one(
@@ -20,27 +20,3 @@ class AccountInvoice(models.Model):
     def onchange_account_wallet_type_id(self):
         if self.account_wallet_type_id:
             self.account_id = self.account_wallet_type_id.account_id
-
-    def invoice_line_move_line_get(self):
-        """
-        Create move line with cagnotte id if needed
-        :return:
-        """
-        res = super(AccountInvoice, self).invoice_line_move_line_get()
-        wallet_lines = self.invoice_line_ids.filtered("account_cagnotte_id")
-        for line_val in res:
-            invl_id = line_val.get("invl_id")
-            if invl_id in wallet_lines.ids:
-                line_val.update({
-                    "account_cagnotte_id": wallet_lines.filtered(
-                        lambda c, l_id=invl_id: c.id == l_id).mapped(
-                        "account_wallet_id").id})
-        return res
-
-    @api.model
-    def line_get_convert(self, line, part):
-        res = super(AccountInvoice, self).line_get_convert(line, part)
-        wallet_id = line.get("account_cagnotte_id")
-        if wallet_id:
-            res.update({"account_wallet_id": wallet_id})
-        return res
