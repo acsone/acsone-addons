9c3629fdfff809bf07665a3a2c361e7a3604ac2f
Denis Roussel <denis.roussel@acsone.eu>
2021-08-23 17:43:56 +0200

    [14.0][MIG] pos_account_wallet_coupon

---
+++ pos_account_wallet_coupon/__manifest__.py
@@ -0,0 +1,30 @@
+# © 2015  Laetitia Gangloff, Acsone SA/NV (http://www.acsone.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "POS Account Wallet Coupon",
+    'version': '14.0.1.0.0',
+    'author': 'Acsone SA/NV, Odoo Community Association (OCA)',
+    'category': "Point Of Sale,Accounting & Finance",
+    'website': 'http://github.com/OCA/account-wallet',
+    "maintainers": ["rousseldenis"],
+    'depends': [
+        'account_wallet_coupon',
+        # TODO:
+        # 'cagnotte_no_negative',
+        'point_of_sale'
+    ],
+    'data': [
+        'views/pos_order.xml',
+        'views/pos_template.xml',
+    ],
+    "demo": [
+        "demo/product_product.xml",
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'pre_init_hook': 'pre_init_hook',
+}
---
+++ pos_account_wallet_coupon/models/product_product.py
@@ -0,0 +1,28 @@
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    is_wallet_with_coupon = fields.Boolean(
+        compute="_compute_is_wallet_with_coupon",
+        help="This is a technical field in order to determine if the"
+        "product is a Wallet one and force the usage of Coupon.")
+
+    def _get_is_wallet_with_coupon_domain(self):
+        return [
+            ('with_coupon_code', '=', True),
+            ("product_id", "in", self.ids)
+        ]
+
+    def _compute_is_wallet_with_coupon(self):
+        WalletTypeObj = self.env['account.wallet.type']
+        wallet_types = WalletTypeObj.search(
+            self._get_is_wallet_with_coupon_domain())
+        products = wallet_types.mapped("product_id")
+        for product in self:
+            product.is_wallet_with_coupon = True if product in products else False
