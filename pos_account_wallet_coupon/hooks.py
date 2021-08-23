f1badb13b45b30084ce45c0bd3f9c7ee9b01cbc8
Denis Roussel <denis.roussel@acsone.eu>
2021-08-23 17:43:56 +0200

    [14.0][MIG] pos_account_wallet_coupon

---
+++ pos_account_wallet_coupon/hooks.py
@@ -0,0 +1,29 @@
+# Copyright 2021 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+from odoo import api, SUPERUSER_ID
+
+
+def _migrate_columns(env):
+    """
+        This is intended solely if former module was installed.
+    """
+    query = """
+        SELECT id FROM ir_module_module WHERE name = 'pos_cagnotte_coupon'
+    """
+    env.cr.execute(query)
+    if env.cr.fetchall():
+        columns = {
+            "account_bank_statement_line": [
+                ("account_bank_statement_line", "account_cagnotte_id", 'account_wallet_id')
+            ],
+            "pos_order_line": [
+                ("account_bank_statement_line", "account_cagnotte_id", 'account_wallet_id')
+            ]}
+        openupgrade.copy_columns(env.cr, columns)
+
+
+def pre_init_hook(cr):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        _migrate_columns(env)
---
+++ pos_account_wallet_coupon/static/src/js/SetCouponCodePopup.js
@@ -0,0 +1,25 @@
+odoo.define('pos_account_wallet_coupon.popups', function(require) {
+    'use strict';
+    var core = require('web.core');
+    var _t = core._t;
+    const { useState, useRef } = owl.hooks;
+    const TextInputPopup = require('point_of_sale.TextInputPopup');
+    const Registries = require('point_of_sale.Registries');
+
+
+    class SetCouponCodePopup extends TextInputPopup {
+        constructor() {
+            super(...arguments);
+        }
+    }
+    SetCouponCodePopup.template = 'SetCouponCodePopup';
+    SetCouponCodePopup.defaultProps = {
+        confirmText: 'Ok',
+        cancelText: 'Cancel',
+        title: _t('Enter Wallet Code'),
+        body: '',
+        startingValue: '',
+    };
+    Registries.Component.add(SetCouponCodePopup);
+    return SetCouponCodePopup;
+});
