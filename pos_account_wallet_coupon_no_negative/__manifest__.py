e0582f486af51faade6ff954890e8e1af087b60e
Denis Roussel <denis.roussel@acsone.eu>
2021-08-25 17:08:43 +0200

    [14.0][ADD] pos_account_wallet_coupon_no_negative

---
+++ pos_account_wallet_coupon_no_negative/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Pos Account Wallet Coupon No Negative',
+    'summary': """
+        Glue module to manage no negative Wallets feature on POS side""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-wallet',
+    'depends': [
+        "pos_account_wallet_coupon",
+        "account_wallet_no_negative",
+    ],
+    'data': [
+        "views/pos_template.xml",
+    ],
+    "auto_install": True,
+}
