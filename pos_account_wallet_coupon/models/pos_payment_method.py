d7d7e4a67f922529a41be50a9b7daf4df9143890
Denis Roussel <denis.roussel@acsone.eu>
2021-10-07 15:53:07 +0200

    [14.0][IMP] pos_account_wallet_coupon: Add contraint for wallet payment method
    If POS payment method is managed through a wallet cash journal, split transactions option
    is mandatory.
    
    Add tests for split transactions and a session with several orders

--- pos_account_wallet_coupon/models/pos_payment_method.py
+++ pos_account_wallet_coupon/models/pos_payment_method.py
@@ -1,12 +1,22 @@
 # Copyright 2021 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import _, api, models, fields
+from odoo.exceptions import ValidationError
 
 
 class PosPaymentMethod(models.Model):
     _inherit = 'pos.payment.method'
 
     is_wallet_with_coupon = fields.Boolean(
-        related="cash_journal_id.is_wallet_with_coupon"
+        related="cash_journal_id.is_wallet_with_coupon",
+        store=True,
     )
+
+    @api.constrains("is_wallet_with_coupon", "split_transactions")
+    def _check_wallet_split_transactions(self):
+        for method in self:
+            if method.is_wallet_with_coupon and not method.split_transactions:
+                raise ValidationError(
+                    _("If the payment method is managed with wallets, "
+                        "you should check 'Split Transactions' too."))
