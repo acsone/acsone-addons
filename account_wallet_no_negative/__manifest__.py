9e230c0d42edd21f9c46e617e6f06bcec82777f8
Denis Roussel <denis.roussel@acsone.eu>
2021-11-09 19:50:36 +0100

    [14.0][IMP] pre-commit

--- account_wallet_no_negative/__manifest__.py
+++ account_wallet_no_negative/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Wallet No Negative',
-    'summary': """
+    "name": "Account Wallet No Negative",
+    "summary": """
         Allows to set a limit on wallet use""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-wallet',
-    'depends': [
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://acsone.eu",
+    "depends": [
         "account_wallet",
     ],
     "data": [
         "views/account_wallet_type.xml",
         "views/account_wallet.xml",
-    ]
+    ],
 }
