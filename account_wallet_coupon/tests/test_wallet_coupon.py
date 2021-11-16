2f08430fd76958713c995de2c42d98159c050310
Denis Roussel <denis.roussel@acsone.eu>
2021-11-16 12:21:32 +0100

    [14.0][IMP] account_wallet_coupon: Use wallet coupon to pay invoice

--- account_wallet_coupon/tests/test_wallet_coupon.py
+++ account_wallet_coupon/tests/test_wallet_coupon.py
@@ -1,9 +1,12 @@
 # Copyright 2015-2021 ACSONE SA/NV (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.addons.account_wallet.tests.common import WalletCommon
+from odoo import fields
+from odoo.tests import Form
+
+from .common import WalletCouponCommon
 
 
-class TestWalletCoupon(WalletCommon):
+class TestWalletCoupon(WalletCouponCommon):
     def test_wallet(self):
         """Create wallet
         Check coupon code is empty
@@ -18,3 +21,28 @@ class TestWalletCoupon(WalletCommon):
         wallet_type.with_coupon_code = True
         wallet = wallet_obj.create({"wallet_type_id": wallet_type.id})
         self.assertTrue(wallet.coupon_id.code)
+
+    def test_payment(self):
+        wallet_type = self.env.ref("account_wallet.wallet_type")
+        wallet_type.with_coupon_code = True
+        wallet = self.wallet_obj.create({"wallet_type_id": wallet_type.id})
+        self._provision_wallet(500.0, wallet=wallet)
+        with Form(
+            self.env["account.move"].with_context(default_move_type="out_invoice")
+        ) as move_form:
+            move_form.partner_id = self.partner
+            move_form.invoice_date = fields.Date.from_string("2019-01-01")
+            with move_form.invoice_line_ids.new() as line_form:
+                line_form.product_id = self.env.ref("product.product_product_9")
+        invoice = move_form.save()
+        invoice._post()
+        with Form(
+            self.payment_obj.with_context(
+                active_ids=invoice.ids, active_model=invoice._name
+            )
+        ) as payment_form:
+            payment_form.journal_id = self.wallet_journal
+            payment_form.coupon_code = wallet.coupon_id.code
+
+        payment = payment_form.save()
+        payment.action_create_payments()
