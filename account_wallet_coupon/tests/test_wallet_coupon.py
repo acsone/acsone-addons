adb877b935ffe3c7557e023e574709521ca88996
Denis Roussel <denis.roussel@acsone.eu>
2021-11-16 15:50:07 +0100

    [14.0][IMP] account_wallet_coupon: Improve test for invoice payment

--- account_wallet_coupon/tests/test_wallet_coupon.py
+++ account_wallet_coupon/tests/test_wallet_coupon.py
@@ -23,8 +23,21 @@ class TestWalletCoupon(WalletCouponCommon):
         self.assertTrue(wallet.coupon_id.code)
 
     def test_payment(self):
+        """
+        Provision wallet with 500.0
+        Create an invoice
+        Pay with the wallet
+        Check the invoice is paid
+        Check the wallet balance equals the initial balance - the paid amount
+        """
         wallet_type = self.env.ref("account_wallet.wallet_type")
         wallet_type.with_coupon_code = True
+        wallet_type.journal_id.payment_credit_account_id = (
+            wallet_type.journal_id.default_account_id
+        )
+        wallet_type.journal_id.payment_debit_account_id = (
+            wallet_type.journal_id.default_account_id
+        )
         wallet = self.wallet_obj.create({"wallet_type_id": wallet_type.id})
         self._provision_wallet(500.0, wallet=wallet)
         with Form(
@@ -36,6 +49,7 @@ class TestWalletCoupon(WalletCouponCommon):
                 line_form.product_id = self.env.ref("product.product_product_9")
         invoice = move_form.save()
         invoice._post()
+        wallet_amount_after = wallet.balance - invoice.amount_total
         with Form(
             self.payment_obj.with_context(
                 active_ids=invoice.ids, active_model=invoice._name
@@ -46,3 +60,6 @@ class TestWalletCoupon(WalletCouponCommon):
 
         payment = payment_form.save()
         payment.action_create_payments()
+
+        self.assertEqual("paid", invoice.payment_state)
+        self.assertEqual(wallet_amount_after, wallet.balance)
