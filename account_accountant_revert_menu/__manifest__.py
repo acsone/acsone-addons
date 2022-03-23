f850f122b5c96166feb7d264e735205688ca5c7f
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-03-23 09:32:41 +0100

    account_accountant_revert_menu: explain why this module is not necessary

--- account_accountant_revert_menu/__manifest__.py
+++ account_accountant_revert_menu/__manifest__.py
@@ -4,7 +4,19 @@
 {
     "name": "Account Accountant Revert Menu",
     "description": """
-        This addon improves the compatibility of Enterprise accounting.""",
+        This addon improves the compatibility of Enterprise accounting.
+
+        A better alternative to this addon is to lookup the parent menu entries like this
+        (credit Martin Trigaux - https://github.com/OCA/l10n-spain/pull/2094#issuecomment-1030125850):
+
+        ```
+        <record id="menu_root_aeat" model="ir.ui.menu">
+            ...
+            <!-- parent of the "Customers" menu -->
+            <field name="parent_id" search="[('child_id', 'in', ref('account.menu_finance_receivables'))]" />
+        </record>
+        ```
+    """,
     "version": "14.0.1.0.0",
     "license": "Other proprietary",
     "author": "ACSONE SA/NV",
