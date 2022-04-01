2dafec50064955165a3f06e660a3c7a2a4d3317d
Denis Roussel <denis.roussel@acsone.eu>
2022-04-01 18:11:46 +0200

    [14.0][ADD] account_followup_print_action

---
+++ account_followup_print_action/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 ACSONE SA/NV
+# License Other proprietary
+{
+    'name': 'Account Followup Print Action',
+    'description': """
+        Allows to print followup letters through an action""",
+    'version': '14.0.1.0.0',
+    'license': 'Other proprietary',
+    'author': 'ACSONE SA/NV',
+    'website': 'https://acsone.eu',
+    'depends': [
+        "account_followup"
+    ],
+    'data': [
+        "data/ir_actions_server.xml",
+    ],
+}
